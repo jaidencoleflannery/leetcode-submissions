1abcdb9615fc256c322f4c5de4e2f904f292f4d3
Cole Flannery <jaidencoleflannery@gmail.com>
2026-04-04 00:23:54 -0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length)
+            return false;
+
+        int[] container = new int[26];
+
+        for(int cursor = 0; cursor < s.Length; cursor++) {
+            container[s[cursor] - 'a']++;
+            container[t[cursor] - 'a']--; 
+        }
+
+        foreach(int quantity in container)
+            if(quantity > 0)
+                return false;
+        return true;
+    }
+}
